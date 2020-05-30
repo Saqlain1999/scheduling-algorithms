75587e9bf10675b1c023f9fb9fdaf26347e40d79
Syed Saqlain Shah <32074101+Saqlain1999@users.noreply.github.com>
2020-05-31 04:54:17 +0500

    Add files via upload

---
+++ FCFS.sh
@@ -0,0 +1,42 @@
+clear
+declare -A processes
+declare -A bTime
+declare -A wTime
+declare -A taTime
+zero=0;
+
+read -p "How many process are you going to add: " psNos
+
+for ((i=0; i<$psNos; i++))
+do
+    read -p "Enter Process Number $i: " processes[$i]
+    read -p "Enter Burst Time for Process Number $i: " bTime[$i]
+    if (( ${bTime[$i]}==$zero ))
+    then
+        while (( ${bTime[$i]}==$zero ))
+        do
+            echo "Burst Time Cannot Be Zero (0)"
+            read -p "Enter Burst Time for Process Number $i: " bTime[$i]
+            if (( ${bTime[$i]}!=$zero ))
+            then
+                 break
+            fi
+        done
+    fi
+done
+let wTime[0]=0
+for ((i=1;i<=$psNos;i++))
+do
+    wTime[$i]=0
+    for ((j=0;j<$i;j++))
+    do
+        wTime[$i]=$((${wTime[$i]} + ${bTime[$j]}))
+    done
+done
+taTime[0]=0
+printf "  Process Name     Burst Time     Waiting Time     Turnaround Time\n"
+for ((i=0;i<$psNos;i++))
+do
+let taTime[$i]=$((${wTime[$i]} + ${bTime[$i]}))
+printf "\t%s\t\t%d\t\t%d\t\t%d\n" ${processes[$i]} ${bTime[$i]} ${wTime[$i]} ${taTime[$i]}
+done
